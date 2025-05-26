cb185ef3d24ddd6149c25c3029a9f3b5b02efe45
Liya Bekyarova <liya.ilieva@gmail.com>
2025-05-27 01:07:39 +0300

    fixed the build

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,54 +6,20 @@ import { ReactNode } from 'react';
 
 const ibmPlexSans = localFont({
   src: [
-    {
-      path: './public/fonts/IBMPlexSans-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './public/fonts/IBMPlexSans-Medium.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: './public/fonts/IBMPlexSans-SemiBold.ttf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: './public/fonts/IBMPlexSans-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
   ],
 });
 
 const bebasNeue = localFont({
   src: [
-    {
-      path: './public/fonts/BebasNeue-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './public/fonts/BebasNeue-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
   ],
-  variable: '--bebas-neue',
-});
- 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--bebas-neue",
 });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'BookWise',
@@ -68,8 +34,8 @@ const RootLayout=({
   return (
     <html lang="en">
       <body
-        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
-      >
+          className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+        >
         {children}
       </body>
     </html>
